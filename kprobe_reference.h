1752cb61a50c5802d8af0c0af15e6a787b01a9b3
fastec02 <fastec0035@icloud.com>
2018-10-29 14:46:37 +0900

    update 2018年 10月 29日 月曜日 14:46:37 JST

---
+++ kprobe_reference.h
@@ -0,0 +1,33 @@
+#include<linux/sched.h>
+
+static void printk_regs(const char* msg,struct kprobe *kp,struct pt_regs *regs);
+static void printk_thread(struct kprobe *kp,struct pt_regs *regs);
+
+static void printk_regs(const char* msg,struct kprobe *kp,struct pt_regs *regs)
+{
+	printk(KERN_INFO "--[KERNEL REGS]\n");
+        printk(KERN_INFO "----[MESG] %s\n",msg);
+        printk(KERN_INFO "----[INFO]p->addr = 0x%p\n",kp->addr);
+        printk(KERN_INFO "----[INFO]ip      = %lx\n",regs->ip);
+        printk(KERN_INFO "----[INFO]<RAX>   = 0x%lx\n",regs->ax);
+        printk(KERN_INFO "----[INFO]<RBX>   = 0x%lx\n",regs->bx);
+        printk(KERN_INFO "----[INFO]<RCX>   = 0x%lx\n",regs->cx);
+        printk(KERN_INFO "----[INFO]<RDX>   = 0x%lx\n",regs->dx);
+}
+
+static void printk_thread(struct kprobe *kp,struct pt_regs *regs)
+{
+	struct task_struct *task;
+	read_lock(&tasklist_lock);
+	for_each_process(task)
+	{
+		printk(KERN_INFO "--[THREAD_INFO]\n");
+		printk(KERN_INFO "----[INFO]<PID>	= %x\n",task->pid);
+		printk(KERN_INFO "----[INFO]<PTR>	= %lx\n",task->thread_info);
+		printk(KERN_INFO "----[INFO]<STATUS>	= %lx\n",task->thread_info.status);
+		printk(KERN_INFO "----[INFO]<FLAGS>	= %lx\n",task->thread_info.flags);
+
+	}
+	read_unlock(&tasklist_lock);
+}
+
