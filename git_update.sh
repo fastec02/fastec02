f4f4ca0e10af12e330c572704175008999b7d168
fastec02 <fastec0035@icloud.com>
2018-10-29 14:55:10 +0900

    update 2018年 10月 29日 月曜日 14:55:10 JST

--- git_update.sh
+++ git_update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git remote set-url --add origin git@github.com:fastec02/fastec02.git
+git remote set-url --delete origin git://github.com/fastec02/fastec02.git
 git add *
 git commit -a -m "update $(date)"
 git push
