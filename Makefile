082434f9642cbe61c6cfc12c1d12e3959f4a90b7
fastec02 <fastec0035@icloud.com>
2018-11-02 02:17:03 +0900

    update 2018年 11月  2日 金曜日 02:17:03 JST

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+install:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	sudo insmod kprobe_test.ko
+	make clean
--- kprobe_reference.h
+++ kprobe_reference.h
@@ -4,10 +4,11 @@
 static void printk_regs(const char* msg,struct kprobe *kp,struct pt_regs *regs);
 static void printk_thread(struct kprobe *kp,struct pt_regs *regs);
 
+static unsigned int sp_register;
 
 static void printk_regs(const char* msg,struct kprobe *kp,struct pt_regs *regs)
 {
-	printk(KERN_INFO "--[KERNEL REGS]\n");
+	printk(KERN_ALERT "--[KERNEL REGS]\n");
         printk(KERN_INFO "----[MESG] %s\n",msg);
         printk(KERN_INFO "----[INFO]p->addr = 0x%p\n",kp->addr);
         printk(KERN_INFO "----[INFO]ip      = %lx\n",regs->ip);
@@ -15,6 +16,8 @@ static void printk_regs(const char* msg,struct kprobe *kp,struct pt_regs *regs)
         printk(KERN_INFO "----[INFO]<RBX>   = 0x%lx\n",regs->bx);
         printk(KERN_INFO "----[INFO]<RCX>   = 0x%lx\n",regs->cx);
         printk(KERN_INFO "----[INFO]<RDX>   = 0x%lx\n",regs->dx);
+	printk(KERN_INFO "----[INFO]<SP>    = 0x%lx\n",regs->sp);
+	sp_register = regs->sp;
 }
 
 static void printk_thread(struct kprobe *kp,struct pt_regs *regs)
@@ -22,14 +25,17 @@ static void printk_thread(struct kprobe *kp,struct pt_regs *regs)
 	struct task_struct *task;
 	rcu_read_lock();
 	list_for_each_entry_rcu(task,&init_task.tasks,tasks){
-		printk(KERN_INFO "--[THREAD_INFO]\n");
-		printk(KERN_INFO "----[INFO]<ADDR>	= %lx\n",&task);
-		printk(KERN_INFO "----[INFO]<FUNCTION>  = %s\n",task->comm);
-		printk(KERN_INFO "----[INFO]<PID>	= %d\n",task->pid);
-		printk(KERN_INFO "----[INFO]<PTR>	= %lx\n",task->thread_info);
-		printk(KERN_INFO "----[INFO]<STATUS>	= %lx\n",task->thread_info.status);
-		printk(KERN_INFO "----[INFO]<FLAGS>	= %lx\n",task->thread_info.flags);
+	//if(sp_register == task->thread.sp){
+			printk(KERN_ALERT "--[THREAD_INFO]\n");
+			printk(KERN_INFO "----[INFO]<ADDR>	= %lx\n",kp->addr);
+			printk(KERN_INFO "----[INFO]<FUNCTION>  = %s\n",task->comm);
+			printk(KERN_INFO "----[INFO]<THREAD>	= %lx\n",task->thread);
+			printk(KERN_INFO "----[INFO]<REG-SP>	= %lx\n",task->thread.sp);
+			printk(KERN_INFO "----[INFO]<PID>	= %d\n",task->pid);
+			printk(KERN_INFO "----[INFO]<PTR>	= %lx\n",task->thread_info);
+			printk(KERN_INFO "----[INFO]<STATUS>	= %lx\n",task->thread_info.status);
+			printk(KERN_INFO "----[INFO]<FLAGS>	= %lx\n",task->thread_info.flags);
+		//}
 	}
 	rcu_read_unlock();
 }
-
