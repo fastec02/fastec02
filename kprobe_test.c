0dd0161607c3c965df8f01540fda88ac3595f9ba
fastec02 <fastec0035@icloud.com>
2018-11-02 15:29:46 +0900

    update 2018年 11月  2日 金曜日 15:29:46 JST

--- kprobe_test.c
+++ kprobe_test.c
@@ -3,14 +3,14 @@
 #include<linux/kprobes.h>
 #include<linux/ptrace.h>
 #include "kprobe_reference.h"
-#define TARGET "_do_fork"
+#define TARGET "do_open"
 MODULE_LICENSE("GPL");
 
 static int pre(struct kprobe *kp,struct pt_regs *regs)
 {
-	//printk(KERN_INFO "[FOOK]PRE FUNCTION '%s'\n",TARGET);
+	printk(KERN_INFO "[FOOK]PRE FUNCTION '%s'\n",TARGET);
 	printk_regs("PRE",kp,regs);
-	printk_thread(kp,regs);
+	//printk_thread(kp,regs);
 	return 0;
 }
 
